5241b25adcb70ac21bd84f02eb1765e0baf70aae
Voronwe <Voronwe.S@protonmail.com>
2021-08-05 18:23:22 +0300

    Bot answers with drug name, description and analogs
    Closes #3

--- bot/management/commands/bot.py
+++ bot/management/commands/bot.py
@@ -1,14 +1,37 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.ext import Updater, CommandHandler, CallbackContext, Filters, MessageHandler
 
 from django.core.management.base import BaseCommand
+
+from bot.models import Drug
 
 
 def start(update: Update, context: CallbackContext) -> None:
     user = update.effective_user
     update.message.reply_markdown_v2(
-            fr'Hi {user.mention_markdown_v2()}\!',
-        )
+        fr'Hi {user.mention_markdown_v2()}\!',
+    )
+
+def find_drug(update: Update, context: CallbackContext) -> None:
+    user_text = update.message.text.lower().capitalize()
+    text = f'По вашему запросу "{user_text}" ничего не найдено'
+
+    try:
+        drug = Drug.objects.get(names__contains=[user_text])
+
+        text_parts = [f'Название: {user_text}']
+
+        if len(drug.names) > 1:
+            analogs = ', '.join(filter(lambda name: name != user_text, drug.names))
+            text_parts.append(f'Аналоги: {analogs}')
+
+        text_parts.append(f'\nОписание: {drug.description}')
+
+        text = '\n'.join(text_parts)
+    except Drug.DoesNotExist:
+        pass
+
+    update.message.reply_text(text)
 
 
 class Command(BaseCommand):
@@ -19,8 +42,11 @@ class Command(BaseCommand):
 
         # TODO: token
         updater = Updater('set token here')
+        dispatcher = updater.dispatcher
 
-        updater.dispatcher.add_handler(CommandHandler('start', start))
+
+        dispatcher.add_handler(CommandHandler('start', start))
+        dispatcher.add_handler(MessageHandler(Filters.text, find_drug))
 
         updater.start_polling()
 
