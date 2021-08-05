8e1631252591abe7f08a36882caed883423c3160
Voronwe <Voronwe.S@protonmail.com>
2021-08-05 20:28:47 +0300

    Add drug categories
    Closes #4

--- bot/admin.py
+++ bot/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
-from bot.models import Drug
+from bot.models import Drug, Category
 
 @admin.register(Drug)
 class DrugAdmin(admin.ModelAdmin):
-    list_display = ('names', 'description',)
+    list_display = ('names',)
     search_fields = ['names',]
+
+
+@admin.register(Category)
+class CategorAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ['name',]
 
--- bot/models.py
+++ bot/models.py
@@ -1,9 +1,19 @@
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    smile = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return f'{self.name} {self.smile}'
+
+
 class Drug(models.Model):
     names = ArrayField(models.CharField(max_length=255, unique=True))
     description = models.TextField()
+    categories = models.ManyToManyField(Category, blank=True)
 
     def __str__(self):
         return f'{self.names}'
